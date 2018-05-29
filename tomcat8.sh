ea1ad3c81bc4bef668657b1910ec585d3847fd31
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-05-29 16:43:54 +0200

    Update tomcat8.sh

--- tomcat8.sh
+++ tomcat8.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install default-jdk
 sudo groupadd tomcat
 sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
-cd /tmp
+
 curl -O http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.5/bin/apache-tomcat-8.5.5.tar.gz
 sudo mkdir /opt/tomcat
 sudo tar xzvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
