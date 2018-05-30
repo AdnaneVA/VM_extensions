09a359b8092e05256765ad079bbd4391e10865f4
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-05-30 15:56:29 +0200

    Update tomcat8.sh

--- tomcat8.sh
+++ tomcat8.sh
@@ -2,3 +2,13 @@ sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y install default-jdk
 sudo apt-get -y install tomcat8
 
+sudo apt-get -y install unixodbc odbcinst
+
+wget https://dev.mysql.com/get/Downloads/Connector-ODBC/5.3/mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit.tar.gz
+
+tar -xvf mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit.tar.gz
+
+cp mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit/lib/libmyodbc5* /usr/lib/x86_64-linux-gnu/odbc/
+
+mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit/bin/myodbc-installer -d -a -n “mysql” -t “DRIVER=/usr/lib/x86_64-linux-gnu/odbc/libmyodbc5w.so;”
+
