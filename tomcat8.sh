fcfe482779cb4fa2372e29aee053fe67f3678f4e
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-05-30 16:15:32 +0200

    Update tomcat8.sh

--- tomcat8.sh
+++ tomcat8.sh
@@ -8,7 +8,7 @@ wget https://dev.mysql.com/get/Downloads/Connector-ODBC/5.3/mysql-connector-odbc
 
 tar -xvf mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit.tar.gz
 
-cp mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit/lib/libmyodbc5* /usr/lib/x86_64-linux-gnu/odbc/
+sudo cp mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit/lib/libmyodbc5* /usr/lib/x86_64-linux-gnu/odbc/
 
-mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit/bin/myodbc-installer -d -a -n “mysql” -t “DRIVER=/usr/lib/x86_64-linux-gnu/odbc/libmyodbc5w.so;”
+sudo mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit/bin/myodbc-installer -d -a -n "mysql" -t "DRIVER=/usr/lib/x86_64-linux-gnu/odbc/libmyodbc5w.so;"
 
