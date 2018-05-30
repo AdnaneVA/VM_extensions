8606d68849ea51a13d3ff475982dd4914e8e1cb2
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-05-30 10:35:54 +0200

    Update tomcat8.sh

--- tomcat8.sh
+++ tomcat8.sh
@@ -5,3 +5,32 @@ sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 wget http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.5/bin/apache-tomcat-8.5.5.tar.gz
 sudo tar -xzvf apache-tomcat-8.5.5.tar.gz
 sudo mv apache-tomcat-8.5.5 /opt/tomcat
+sudo chgrp -R tomcat /opt/tomcat
+sudo chown -R tomcat /opt/tomcat
+sudo chmod -R 755 /opt/tomcat
+sudo nano /etc/systemd/system/tomcat.service
+[Unit]
+Description=Apache Tomcat Web Server
+After=network.target
+
+[Service]
+Type=forking
+
+Environment=JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat
+Environment=CATALINA_BASE=/opt/tomcat
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdown.sh
+
+User=tomcat
+Group=tomcat
+UMask=0007
+RestartSec=15
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
