a1561f3dfd3564109ee264bafc0b3640d304e336
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-05-30 10:15:47 +0200

    Update tomcat8.sh

--- tomcat8.sh
+++ tomcat8.sh
@@ -1,21 +1,7 @@
-sudo apt-get install python-software-properties -y
-sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer -y
-vim /etc/environment
-JAVA_HOME="/usr/lib/jvm/java-8-oracle/jre"
-vim ~/.bashrc
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre
-export PATH=$JAVA_HOME/bin:$PATH
-source ~/.bashrc
-groupadd tomcat
-useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
-cd /opt/
-wget http://mirror.wanxp.id/apache/tomcat/tomcat-8/v8.5.6/bin/apache-tomcat-8.5.6.tar.gz
-tar -xzvf apache-tomcat-8.5.6.tar.gz
-mv apache-tomcat-8.5.6 tomcat
-chown -hR tomcat:tomcat tomcat
-chmod +x /opt/tomcat/bin/*
-vim ~/.bashrc
-export CATALINA_HOME=/opt/tomcat
-source ~/.bashrc
+sudo apt-get install default-jdk
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+wget http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.5/bin/apache-tomcat-8.5.5.tar.gz
+sudo tar -xzvf apache-tomcat-8.5.5.tar.gz
+sudo mv apache-tomcat-8.5.5 /opt/tomcat
