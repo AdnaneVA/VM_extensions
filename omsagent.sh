4d557eea1b6e7b8583cb2909b78c335239c314c1
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-05-31 14:56:25 +0200

    Update omsagent.sh

--- omsagent.sh
+++ omsagent.sh
@@ -1,3 +1,5 @@
-sudo wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sudo sh onboard_agent.sh -w 3104b405-8bf6-4c51-a97a-d28f4d9b09a8 -s bPKxwQyOPQwZHQ108COoP+N8JHAuIe2IVJ/IAf8Q5hExGgTd61L8aTPrE49lUIGUmT2czpEKQQJo/Wu1GEl68g== -d opinsights.azure.com
+sudo wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sudo sh onboard_agent.sh -w e4521f02-0541-4c5a-b2e0-800067933dd8 -s kNiBLj+WUXgL5Y2/x/zn6W5UT8auHSqX6diSaESWApF2y5BLTjM9K2by+na0zqjGenpBCu/RHIs/LRTYxJKdug== -d opinsights.azure.com
 cd /opt/microsoft/omsagent/bin
 sudo ./omsadmin.sh
+
+
