d099990ddb3c0980a6bf8c213f2751dc39654487
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-05-31 14:15:44 +0200

    Create omsagent.sh

---
+++ omsagent.sh
@@ -0,0 +1,3 @@
+sudo wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sudo  sh onboard_agent.sh -w 3104b405-8bf6-4c51-a97a-d28f4d9b09a8 -s bPKxwQyOPQwZHQ108COoP+N8JHAuIe2IVJ/IAf8Q5hExGgTd61L8aTPrE49lUIGUmT2czpEKQQJo/Wu1GEl68g== -d opinsights.azure.com
+sudo cd /opt/microsoft/omsagent/bin
+sudo ./omsadmin.sh
