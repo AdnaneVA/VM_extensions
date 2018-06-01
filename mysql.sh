64560f94a5bb5476a00e8c1b997411793b67cce1
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-06-01 10:10:08 +0200

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -2,3 +2,9 @@ apt-get update -y
 apt-get install -y expect
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -q -y install mysql-server
+
+
+#oms agent
+sudo wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sudo sh onboard_agent.sh -w 56ab8047-e4c8-45f5-bcd7-331fee82e9ff -s eGbaJEt9ENdYUnTP5L4CYf4FRj6DQhtAHeeTzV6dB+80PNHlkKlINjVON9xNs1He1q9teGSavntMmAzhuwSstA== -d opinsights.azure.com
+cd /opt/microsoft/omsagent/bin
+sudo ./omsadmin.sh
