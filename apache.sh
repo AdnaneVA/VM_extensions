6885966731c2837005e50c8824d3d91428bab5ce
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-06-01 10:09:42 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,2 +1,7 @@
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y install apache2
+
+#oms agent 
+sudo wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sudo sh onboard_agent.sh -w 56ab8047-e4c8-45f5-bcd7-331fee82e9ff -s eGbaJEt9ENdYUnTP5L4CYf4FRj6DQhtAHeeTzV6dB+80PNHlkKlINjVON9xNs1He1q9teGSavntMmAzhuwSstA== -d opinsights.azure.com
+cd /opt/microsoft/omsagent/bin
+sudo ./omsadmin.sh
