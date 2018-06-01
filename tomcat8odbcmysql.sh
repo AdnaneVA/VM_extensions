ad1584fe64b0fb58c1cc397d303491cda5457057
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-06-01 10:20:08 +0200

    Update tomcat8odbcmysql.sh

--- tomcat8odbcmysql.sh
+++ tomcat8odbcmysql.sh
@@ -12,3 +12,7 @@ sudo cp mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit/lib/libmyodbc5* 
 
 sudo mysql-connector-odbc-5.3.10-linux-ubuntu16.04-x86-64bit/bin/myodbc-installer -d -a -n "mysql" -t "DRIVER=/usr/lib/x86_64-linux-gnu/odbc/libmyodbc5w.so;"
 
+#oms agent 
+sudo wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sudo sh onboard_agent.sh -w 56ab8047-e4c8-45f5-bcd7-331fee82e9ff -s eGbaJEt9ENdYUnTP5L4CYf4FRj6DQhtAHeeTzV6dB+80PNHlkKlINjVON9xNs1He1q9teGSavntMmAzhuwSstA== -d opinsights.azure.com
+cd /opt/microsoft/omsagent/bin
+sudo ./omsadmin.sh
